5473f57a12911c2a6c4fcf8778a5155680bb1527
migmarti <mmart96@hotmail.com>
2018-02-02 16:46:23 -0800

    Add files via upload

---
+++ ValidateEmail.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormField {
+    class ValidateEmail : IValidable {
+        public bool validate(String value) {
+            return value.Contains('@');
+        }
+    }
+}
