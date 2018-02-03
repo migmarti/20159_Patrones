946100000f1170b3733f20703caf5a629a37eda0
migmarti <mmart96@hotmail.com>
2018-02-02 16:54:10 -0800

    Add files via upload

---
+++ Tareas/Tarea1/FormField/FormField.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormField {
+    abstract class FormField : IValidable {
+        public IValidable validationType;
+        public bool isValid;
+        public string value;
+        
+        public abstract bool validate(String value);
+    }
+}
---
+++ Tareas/Tarea1/FormField/StringField.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormField {
+    class StringField : FormField {
+        public StringField(IValidable validationType, string value) : base() {
+            this.validationType = validationType;
+            this.value = value;
+        }
+        override public bool validate(String value) {
+            return validationType.validate(value);
+        }
+        public void testMessage() {
+            Console.WriteLine("Is " + this.value + " valid? " + this.validate(this.value));
+        }
+    }
+}
