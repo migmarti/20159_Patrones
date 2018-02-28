7fdcaf4ab60b85c0b00e1caa2656d6969606704e
Unknown <mmart96@hotmail.com>
2018-02-28 14:42:47 -0800

    Practica 7

---
+++ Practica7/Practica7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Practica7 {
+    class Program {
+        static void Main(string[] args) {
+
+            StreamReader sr = new StreamReader("test.txt");
+            Uppercaser file = new Uppercaser(sr);
+            file.print();
+  
+            Console.ReadLine();  
+        }
+    }
+}
---
+++ Practica7/Practica7/Uppercaser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica7 {
+    class Uppercaser : TextReader {
+
+        StreamReader sr;
+        public Uppercaser(StreamReader sr) {
+            this.sr = sr;
+        }
+
+        public override string ReadLine() {
+            string line = sr.ReadLine();
+            if (line != null) {
+                line = line.ToUpper();
+            }
+            return line;
+        }
+
+        public void print() {
+            String line;
+            while ((line = ReadLine()) != null) {
+                Console.WriteLine(line);
+            }
+            sr.Close();
+        }
+    }
+}
